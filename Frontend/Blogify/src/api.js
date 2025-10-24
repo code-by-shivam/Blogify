b7672b22e127664413f3b0a62ca402e158a04673
Shivam Chaurasiya <shivam825825@gmail.com>
2025-10-25 01:06:26 +0530

    Preparing for deployment

--- Frontend/Blogify/src/api.js
+++ Frontend/Blogify/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
-export const BASE_URL = "http://127.0.0.1:8001/"
+export const BASE_URL = import.meta.env.VITE_BASE_URL
 
 const api = axios.create({
     baseURL: BASE_URL,
