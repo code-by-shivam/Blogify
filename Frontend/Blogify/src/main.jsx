43b4628c019d3803aa63c5877270adeed87f12c3
Shivam Chaurasiya <shivam825825@gmail.com>
2026-01-16 00:06:37 +0530

    fix router + spa routing

--- Frontend/Blogify/src/main.jsx
+++ Frontend/Blogify/src/main.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css"; // ✅ MUST
+
+
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
