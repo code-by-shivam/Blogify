bdd283415142f31c0e15c43d3877a9d0311497ae
Shivam Chaurasiya <shivam825825@gmail.com>
2026-01-15 23:16:29 +0530

    fix router + spa routing

--- Frontend/Blogify/src/App.jsx
+++ Frontend/Blogify/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import DetailPage from "./pages/DetailPage";
 import { HomePage } from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
