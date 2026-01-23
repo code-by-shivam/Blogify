c9dd51f46495bd5857f63e9d5ef9e1a029e473fc
Shivam Chaurasiya <shivam825825@gmail.com>
2026-01-23 23:35:14 +0530

    Fix Error

--- Frontend/Blogify/src/ui_components/Modal.jsx
+++ Frontend/Blogify/src/ui_components/Modal.jsx
@@ -1,15 +1,14 @@
 const Modal = ({ children, toggleModal }) => {
 
-    function handleToggleModal(e){
-        if(e.target.id === "modal"){
-            toggleModal()
-        }
+  function handleToggleModal(e) {
+    if (e.target.id === "modal") {
+      toggleModal()
     }
+  }
 
   return (
     <div id="modal" onClick={handleToggleModal}
-      className="fixed inset-0 backdrop-blur-md bg-black/30 flex justify-center items-center z-50
-"
+      className="fixed inset-0 backdrop-blur-md bg-black/30 z-50 flex justify-center overflow-y-auto py-10"
     >
       {children}
     </div>
